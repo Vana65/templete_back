502b52faf8d58a458d098d6fada1d5ff28abedea
vana <vanazakaria73@gmail.com>
2025-07-24 16:18:25 +0300

    add subscriber and contact

--- routes/web.php
+++ routes/web.php
@@ -1,8 +1,12 @@
 <?php
 
+use App\Http\Controllers\ContactController;
 use App\Http\Controllers\ProfileController;
 use App\Http\Controllers\ThemeController;
+use App\Models\Contact;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\SubscriberController;
+
 
 /*
 |--------------------------------------------------------------------------
@@ -18,10 +22,12 @@ Route::controller(ThemeController::class)->name('theme.')->group(function () {
     Route::get('/', 'index')->name('index');
     Route::get('/category','category')->name('category');
     Route::get('/contact','contact')->name('contact');
-Route::get('/login','login')->name('login');
-Route::get('/register','register')->name('register');
+//Route::get('/login','login')->name('login');
+//Route::get('/register','register')->name('register');
 Route::get('/singleblog','singleblog')->name('singleblog');
 });
+Route::post('/subscriber/store', [SubscriberController::class, 'store'])->name('subscriber.store');
+Route::post('/contact/store', [ContactController::class, 'store'])->name('contact.store');
 
 Route::get('/dashboard', function () {
     return view('dashboard');
